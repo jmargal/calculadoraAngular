ab45cedda2b4a1a32bc6ffaeeafa039e0da17481
Javier Martinez Gallego (ext) <javier.martinez.external@atos.net>
2023-04-17 12:01:18 +0200

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['button { margin: 0.2em; }'],
 })
 export class AppComponent {
   title = 'primerAngular';
+
+  numero1!: number;
+  numero2!: number ;
+  resultado!: number;
+
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numero1 * this.numero2;
+  }
+
+  dividir(): void {
+    if (this.numero2 != 0) {
+      this.resultado = this.numero1 / this.numero2;
+    } else {
+      alert('No se puede dividir entre 0');
+    }
+  }
 }
